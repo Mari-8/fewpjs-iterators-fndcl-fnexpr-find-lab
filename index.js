1a910e7b19a482c2ef6545d72a7ea32ab96b5498
Mari-8 <ryanaltimari8@gmail.com>
2021-03-15 12:00:53 -0500

    commit tree 4b803532e1711e1a9db5e0b3e2947c2c3f2a7820

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+function superbowlWin(wins) {
+   let winningYear
+    wins.find(function(e) { 
+        if (e.result === "W") {
+            console.log(e.year)
+            winningYear = e.year
+        }
+    })
+    return winningYear
+}
+
